ed89f0d1125b754470598b2851b82b8897a5bbf9
SMD283-TuanNguyen <tuan.nguyenduy@smartdev.com>
2022-10-17 14:34:56 +0700

    handle add image á»url

--- app/src/main/java/com/labdemo/logbookexercise3/ImageDataBase.java
+++ app/src/main/java/com/labdemo/logbookexercise3/ImageDataBase.java
@@ -12,7 +12,7 @@ import androidx.annotation.Nullable;
 public class ImageDataBase extends SQLiteOpenHelper {
 
     private final Context context;
-    private static final  int DATABASE_VERSION = 1;
+    private static final  int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "Image_URL.db";
 
     private static final String TABLE_IMAGE = "ImageURL";
--- app/src/main/java/com/labdemo/logbookexercise3/MainActivity.java
+++ app/src/main/java/com/labdemo/logbookexercise3/MainActivity.java
@@ -2,21 +2,31 @@ package com.labdemo.logbookexercise3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageDataBase imageDataBase;
 
     Button backward_btn;
     Button forward_btn;
     ImageView view_image_item;
     Button add_url_image_btn;
     TextInputLayout input_url_image;
+    ArrayList<String> imageUrlList;
+    int countImageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,35 @@ public class MainActivity extends AppCompatActivity {
         backward_btn = findViewById(R.id.backwardBtn);
         forward_btn = findViewById(R.id.forwardBtn);
 
+        imageDataBase = new ImageDataBase(this);
+
+        getAllImage();
+        countImageUrl = imageUrlList.size();
+        Log.i("value", String.valueOf(countImageUrl));
+        if (imageUrlList.size() == 0) {
+            Glide.with(MainActivity.this).load("https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg?w=2000").into(view_image_item);
+        } else {
+            Glide.with(MainActivity.this).load(imageUrlList.get(1)).into(view_image_item);
+        }
+        input_url_image.getEditText().setText("https://vn-live-02.slatic.net/p/1e627bd5ff074cc9aaf42fb779869590.jpg");
+        handleInsertImageUrl();
+    }
+
+    private void getAllImage() {
+        String urlImageNoData = "https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg?w=2000";
+        Cursor cursor = imageDataBase.displayAllImage();
+        imageUrlList = new ArrayList<>();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(getApplicationContext(), "No Data", Toast.LENGTH_SHORT).show();
+            Glide.with(MainActivity.this).load(urlImageNoData).into(view_image_item);
+        } else {
+            while (cursor.moveToNext()) {
+                imageUrlList.add(cursor.getString(1));
+            }
+        }
+    }
+
+    private void handleInsertImageUrl() {
         add_url_image_btn = findViewById(R.id.buttonAddUrl);
         add_url_image_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,12 +76,15 @@ public class MainActivity extends AppCompatActivity {
                 if (imageURl.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please enter Image URL !!!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Image URL !!!", Toast.LENGTH_SHORT).show();
-//                    imageList.add(imageURl);
-//                    Glide.with(MainActivity.this).load(imageURl).into(view_image_item);
+                    addImageUrl(imageURl);
                     input_url_image.getEditText().setText("");
                 }
             }
         });
+    }
+
+    private void addImageUrl(String url) {
+        ImageDataBase db = new ImageDataBase(this);
+        db.addNewImageUrl(url);
     }
 }
